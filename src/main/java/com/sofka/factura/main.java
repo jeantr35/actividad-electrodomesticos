35b6480a741bc0618cc0f4724a0beaeb8410ebe9
María Fernanda Monroy <mafe2218monroy@gmail.com>
2021-09-02 18:45:47 -0500

    creado el método seleccionarTelevisor en la clase main

--- src/main/java/com/sofka/factura/main.java
+++ src/main/java/com/sofka/factura/main.java
@@ -15,6 +15,41 @@ public class main {
                 "3) Otros\n" +
                 "");
 
-        int tipoElectro = scanner.nextInt();
+        char tipoElectro = scanner.next().charAt(0);
+//        if(tipoElectro == 1){
+//
+//        }
+
+
     }
+    public double seleccionTelevisor() {
+
+        char consumo; String procedencia; int pulgadas;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el consumo (A,B,C): ");
+        consumo = scanner.next().charAt(0);
+        System.out.println("Ingrese la procedencia (nacional,importado): ");
+        procedencia = scanner.next();
+        System.out.println("Ingrese las pulgadas: ");
+        pulgadas = scanner.nextInt();
+        System.out.println("¿Tiene sintonizador de TDT? (si/no)");
+        String esTDT = scanner.next();
+        boolean tdt = tieneSintonizador(esTDT);
+        Televisor televisor = new Televisor(consumo, procedencia, pulgadas, tdt);
+        double precio = televisor.calcularPrecio();
+
+        return precio;
+    }
+    public boolean tieneSintonizador(String esTDT){
+
+        boolean tdt = false;
+        if (esTDT == "si") {
+            tdt = true;
+        } else if (esTDT == "no") {
+            tdt = false;
+        }
+        return tdt;
+    }
+
+
 }
