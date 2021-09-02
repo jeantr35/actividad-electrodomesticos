e8b4953c7b5842161e990f89181ce9dc112c2d4d
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-02 16:34:25 -0500

    Iniciado el menu

--- src/main/java/com/sofka/factura/main.java
+++ src/main/java/com/sofka/factura/main.java
@@ -5,6 +5,16 @@ public class main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Por favor ingrese el numero de items a comprar: ");
+        int cantidad = scanner.nextInt();
 
+        System.out.print("Por favor ingrese el tipo de electrodomestico: \n" +
+                "1) Televisor \n" +
+                "2) Nevera\n" +
+                "3) Otros\n" +
+                "");
+
+        int tipoElectro = scanner.nextInt();
     }
 }
