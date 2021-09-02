c1cdcac7ac2e21d5e228071db231ac5cfec186a6
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-02 12:10:26 -0500

    Creada la clase Nevera y anadido precio a electrodomesticos

--- src/main/java/electrodomesticos.java
+++ src/main/java/electrodomesticos.java
@@ -1,6 +1,13 @@
 public class electrodomesticos {
     char consumo;
     String procedencia;
+    double precio;
+
+    public electrodomesticos() {
+        this.consumo = 'A';
+        this.procedencia = "nacional";
+        this.precio = 0.00;
+    }
 
     public electrodomesticos(char consumo, String procedencia) {
         this.consumo = consumo;
@@ -21,5 +28,31 @@ public class electrodomesticos {
 
     public String getProcedencia() {
         return procedencia;
+    }
+}
+
+class Nevera extends electrodomesticos{
+    int capacidad;
+
+    public Nevera(char consumo, String procedencia, int capacidad) {
+        this.consumo = consumo;
+        this.procedencia = procedencia;
+        this.capacidad = capacidad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double valorExtra(double precio){
+        if (this.capacidad > 120){
+            int extra = (this.capacidad - 120) / 10;
+            return precio * 0.05 * (extra);
+        }
+        else{return 0.00;}
     }
 }
