36f8801d0c09d7fca6dd60e99400904f95b3beed
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-02 13:03:12 -0500

    Creado el metodo calcularPrecio en la clase nevera

--- src/main/java/com/sofka/factura/electrodomesticos.java
+++ src/main/java/com/sofka/factura/electrodomesticos.java
@@ -61,3 +61,34 @@ public class electrodomesticos {
         return this.precio;
     }
 }
+
+class Nevera extends electrodomesticos{
+    int capacidad;
+
+    public Nevera(char consumo, String procedencia, int capacidad) {
+        this.consumo = consumo;
+        this.procedencia = procedencia;
+        this.capacidad = capacidad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public double valorExtra(double precio){
+        if (this.capacidad > 120){
+            int extra = (this.capacidad - 120) / 10;
+            return precio * 0.05 * (extra);
+        }
+        else{return 0.00;}
+    }
+
+    public double calcularPrecio(){
+        return super.calcularPrecio() + valorExtra(super.calcularPrecio());
+    }
+
+}
