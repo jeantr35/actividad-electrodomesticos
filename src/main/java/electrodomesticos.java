7e9b2b86c93474f541f1a3e4b784a081bab48b70
jeantr35 <jean.trujillo.35@gmail.com>
2021-09-02 12:24:08 -0500

    Agredado metodo calcular precio a la clase electrodomesticos

--- src/main/java/electrodomesticos.java
+++ src/main/java/electrodomesticos.java
@@ -12,6 +12,7 @@ public class electrodomesticos {
     public electrodomesticos(char consumo, String procedencia) {
         this.consumo = consumo;
         this.procedencia = procedencia;
+        this.precio = 0.00;
     }
 
     public void setConsumo(char consumo) {
@@ -29,30 +30,33 @@ public class electrodomesticos {
     public String getProcedencia() {
         return procedencia;
     }
-}
 
-class Nevera extends electrodomesticos{
-    int capacidad;
-
-    public Nevera(char consumo, String procedencia, int capacidad) {
-        this.consumo = consumo;
-        this.procedencia = procedencia;
-        this.capacidad = capacidad;
-    }
-
-    public int getCapacidad() {
-        return capacidad;
-    }
-
-    public void setCapacidad(int capacidad) {
-        this.capacidad = capacidad;
-    }
-
-    public double valorExtra(double precio){
-        if (this.capacidad > 120){
-            int extra = (this.capacidad - 120) / 10;
-            return precio * 0.05 * (extra);
+    public double calcularPrecio(){
+        switch (this.consumo){
+            case 'A': {
+                this.precio += 450000;
+                break;
+            }
+            case 'B': {
+                this.precio += 350000;
+                break;
+            }
+            case 'C': {
+                this.precio += 250000;
+                break;
+            }
         }
-        else{return 0.00;}
+
+        switch (procedencia){
+            case "nacional":{
+                this.precio += 250000;
+                break;
+            }
+            case "importado":{
+                this.precio += 350000;
+                break;
+            }
+        }
+        return this.precio;
     }
 }
